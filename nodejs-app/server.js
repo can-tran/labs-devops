adf5eea7f0cb8f71bfd98064207560310e3e3285
Can Tran <can.tran@setacinq.vn>
2024-01-22 13:10:40 +0700

    Update

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -5,10 +5,11 @@ var uuid = require("uuid");
 // var os = require("os");
 var app = express();
 var port = 3000;
+const instanceId = uuid.v4();
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${port} - instance: ${instanceId}`);
 });
 
 app.get("/", (req, res, next) => {
-    res.send(`Welcome to nodejs app service! (ENV: ${process.env.ENV}) - ${uuid.v4()}`)
+    res.send(`Welcome to nodejs app service! (ENV: ${process.env.ENV}) - ${instanceId}`)
 });
